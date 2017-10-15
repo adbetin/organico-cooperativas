ef871b77a14d66004b5d71afbb0f17a75e8ca884
torres <31191475+torres375@users.noreply.github.com>
2017-10-15 13:27:21 -0500

    Create crearCooperativa2.component.ts

---
+++ index/static/organico-app/src/app/cooperativa/crearCooperativa2.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { CrearCooperativaService } from './crearCooperativa.service';
+
+@Component({
+  //selector: 'app-crearCooperativa',
+  templateUrl: './crearCooperativa.component.html',
+  providers: [
+    CrearCooperativaService
+  ]
+})
+export class CrearCooperativaComponent{
+  title = 'Crear cooperativa';
+  envioFormCooperativa = false;
+
+  constructor(private crearCooperativaServices: CrearCooperativaService, private router: Router ){
+
+  }
+
+  guardarCooperativa( formCooperativa: NgForm ) {
+    //console.log(formCooperativa.value);
+    //console.log(formCooperativa.valid);
+    this.envioFormCooperativa = true;
+    if (formCooperativa.valid) {
+      let resultado = this.crearCooperativaServices.guardarCooperativa(formCooperativa.value).subscribe();
+      if( resultado ){
+        alert("Datos guardados correctamente");
+        this.router.navigateByUrl('cooperativa/listadoCooperativa');
+      }else{
+        alert("Error almacenando datos");
+      }
+    }else{
+      //alert("Por favor llene todos los campos que son obligatorios. (*) ");
+    }
+  }
+
+}
