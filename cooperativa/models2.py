e194bdef6fc7d4403480168cc067e1715546fb8b
torres <31191475+torres375@users.noreply.github.com>
2017-10-15 13:14:10 -0500

    Create models2.py

---
+++ cooperativa/models2.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+class Cooperativa(models.Model):
+    nombre = models.CharField(max_length=70)
+    descripcion = models.CharField(max_length=550)
+    zona = models.CharField(max_length=70)
+    responsable = models.CharField(max_length=70)
+    nit = models.CharField(max_length=70)
+    correo = models.CharField(max_length=70)
+    telefono = models.CharField(max_length=70)
+    direccion = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Cooperativa'
+        verbose_name_plural = 'Cooperativas'
+
+    def __str__(self):
+        return '%s' % (self.nombre)
+
+    def __unicode__(self):
+        return self.nombre
