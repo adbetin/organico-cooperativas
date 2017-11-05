c2fc895864a422915a13f14fe13a65977bfd57ef
juesoslo <je.osoriol@uniandes.edu.co>
2017-11-05 18:55:46 -0500

    Rojo: Agregar campos al formulario

--- cooperativa/TDD_test.py
+++ cooperativa/TDD_test.py
@@ -5,7 +5,7 @@ __author__ = 'asistente'
 from unittest import TestCase
 from selenium import webdriver
 
-from selenium import webbrowser
+#from selenium import webbrowser
 
 from selenium.webdriver.common.by import By
 
@@ -22,3 +22,24 @@ class TDDTest(TestCase):
         self.browser.get('http://localhost:8000/cooperativa/servicio/crearServicio')
         h1 = self.browser.find_element(By.XPATH, '//h1[text()="Publicar Servicio"]')
         self.assertIn('Publicar Servicio', h1.text)
+
+    def test_llenarcampos(self):
+        self.browser.get('http://localhost:8000/cooperativa/servicio/crearServicio')
+
+        self.browser.find_element_by_xpath(
+            "//select[@id='cooperativa']/option[text()='cooperativa']").click()
+
+        titulo = self.browser.find_element_by_id('titulo')
+        titulo.send_keys('Próximos eventos del mes de Octubre')
+
+        descripcion = self.browser.find_element_by_id('descripcion')
+        descripcion.send_keys('En el mes de octubre tendremos emocionantes eventos')
+
+        foto = self.browser.find_element_by_id('foto')
+        foto.send_keys("C:\\chromedriver\\blog-image-4.jpg")
+
+        botonGrabar = self.browser.find_element_by_id('guardar')
+        botonGrabar.click()
+
+        span = self.browser.find_element(By.XPATH, '//span[text()="Servicio publicado exitosamente"]')
+        self.assertIn('Servicio publicado exitosamente', span.text)
