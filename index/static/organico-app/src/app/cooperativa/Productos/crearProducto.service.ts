06cf3a5d25e46b1e104477a0f700af69173274e6
torres <31191475+torres375@users.noreply.github.com>
2017-11-22 00:15:42 -0500

    Update crearProducto.service.ts

--- index/static/organico-app/src/app/cooperativa/Productos/crearProducto.service.ts
+++ index/static/organico-app/src/app/cooperativa/Productos/crearProducto.service.ts
@@ -20,7 +20,7 @@ export class CrearProductoService {
       let value = "; " + document.cookie;
       let parts = value.split("; " + name + "=");
       if (parts.length == 2)
-        return parts.pop().split(";").shift();
+        return parts.pop().split(";").shift(); 
     }
 
 }
