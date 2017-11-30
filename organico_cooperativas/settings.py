6fa889c4aba83202853b9bfc3ac49c97b8903b09
Unknown <ad.betin@uniandes.edu.co>
2017-11-30 06:30:53 -0500

    Se modifica el servicio de backend para el worker de channels

--- organico_cooperativas/settings.py
+++ organico_cooperativas/settings.py
@@ -155,11 +155,11 @@ REST_FRAMEWORK = {
 
 CHANNEL_LAYERS = {
    "default": {
-        'BACKEND': 'asgiref.inmemory.ChannelLayer',
-        #"BACKEND": "asgi_redis.RedisChannelLayer",
-        #"CONFIG": {
-        #    "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        #},
+        #'BACKEND': 'asgiref.inmemory.ChannelLayer',
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
         "ROUTING": "organico_cooperativas.routing.channel_routing",
     },
 }
